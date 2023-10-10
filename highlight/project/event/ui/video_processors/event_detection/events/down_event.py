453f7ac38bf3d926a8aa4738de621c626a907154
shmox11 <94550618+shmox11@users.noreply.github.com>
2023-10-10 18:17:23 -0400

    Add files via upload

--- highlight/project/event/ui/video_processors/event_detection/events/down_event.py
+++ highlight/project/event/ui/video_processors/event_detection/events/down_event.py
@@ -1,27 +1,27 @@
 import os
 import cv2
-import sys
-sys.path.append("/Users/ronschmidt/Applications/highlight/project/event/ui/video_processors/event_detection")
-from template_manager import TemplateManager  # Import the TemplateManager class
+from ..regions.center_region import CenterRegion
 
 class DownEvent:
-    def __init__(self, threshold=0.7):
+    def __init__(self, threshold=0.7, templates=None):
         self.threshold = threshold
-        # Create an instance of TemplateManager and access the templates for "center_down_icon"
-        self.templates = TemplateManager().templates['center_down_icon']
+        self.templates = templates if templates else self._load_templates("center_down_icon")
 
-    def _get_roi_gray(self, frame):
-        """Get the region of interest (ROI) in grayscale."""
-        height, width = frame.shape[:2]
-        roi = frame[int(height*0.45):int(height*0.55), int(width*0.45):int(width*0.55)]
-        return cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    def _load_templates(self, event_type):
+        """Load templates for the given event type."""
+        template_dir = os.path.join("thumbnail", event_type)
+        print(f"Attempting to access directory: {template_dir}")
+        return [(cv2.imread(os.path.join(template_dir, path), cv2.IMREAD_GRAYSCALE), os.path.join(template_dir, path)) for path in os.listdir(template_dir) if path.endswith('.png')]
 
     def detect_down_event(self, frame):
         """Detect the 'Down' event using template matching."""
-        roi_gray = self._get_roi_gray(frame)
-        for template in self.templates:
+        self.location = None  # Initialize location to None
+        roi_gray = CenterRegion.get_roi(frame)  # Get the grayscale ROI using the CenterRegion class
+        for template, template_path in self.templates:
             result = cv2.matchTemplate(roi_gray, template, cv2.TM_CCOEFF_NORMED)
             _, max_val, _, max_loc = cv2.minMaxLoc(result)
             if max_val > self.threshold:
-                return True, max_loc
-        return False, None
+                self.location = max_loc  # Store the location
+                print(f"Matched with template: {template_path}")  # Print the path of the matched template
+                return True
+        return False
