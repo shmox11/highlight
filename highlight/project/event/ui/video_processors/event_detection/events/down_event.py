405de74b3847d8264663faafe79b11ad60fd1897
shmox11 <94550618+shmox11@users.noreply.github.com>
2023-10-12 10:57:05 -0400

    Add files via upload

--- highlight/project/event/ui/video_processors/event_detection/events/down_event.py
+++ highlight/project/event/ui/video_processors/event_detection/events/down_event.py
@@ -1,27 +1,72 @@
-import os
-import cv2
-from ..regions.center_region import CenterRegion
+# Import necessary libraries and modules
+import cv2  # OpenCV library for computer vision tasks
+import os  # Library for interacting with the operating system
+from ...event_detection.regions.center_roi import get_center_roi  # Import the function to get the center region of interest (ROI)
+from ...event_detection.preprocessing import normalize_image  # Import the function to normalize an image
+import logging  # Library for logging information
 
-class DownEvent:
-    def __init__(self, threshold=0.7, templates=None):
-        self.threshold = threshold
-        self.templates = templates if templates else self._load_templates("center_down_icon")
+# Set the logging level to INFO to display informational messages
+logging.basicConfig(level=logging.INFO)
 
-    def _load_templates(self, event_type):
-        """Load templates for the given event type."""
-        template_dir = os.path.join("thumbnail", event_type)
-        print(f"Attempting to access directory: {template_dir}")
-        return [(cv2.imread(os.path.join(template_dir, path), cv2.IMREAD_GRAYSCALE), os.path.join(template_dir, path)) for path in os.listdir(template_dir) if path.endswith('.png')]
+# Define a function to get templates of the "down" icon
+def get_down_icon_templates():
+    # Get the directory path of the current script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # Construct the path to the directory containing "down" icon templates
+    template_dir = os.path.join(script_dir, 'thumbnail', 'center_down_icon')
+    
+    # Read all the image templates from the directory and store them in a list
+    templates = [cv2.imread(os.path.join(template_dir, f)) for f in os.listdir(template_dir) if f.endswith('.png')]
+    
+    # Return the list of templates
+    return templates
 
-    def detect_down_event(self, frame):
-        """Detect the 'Down' event using template matching."""
-        self.location = None  # Initialize location to None
-        roi_gray = CenterRegion.get_roi(frame)  # Get the grayscale ROI using the CenterRegion class
-        for template, template_path in self.templates:
-            result = cv2.matchTemplate(roi_gray, template, cv2.TM_CCOEFF_NORMED)
+# Define a function to detect the "down" event in a video frame
+def detect_down_event(frame, threshold=0.8):
+    # Extract the center region of interest (ROI) from the frame
+    roi, top_left, bottom_right = get_center_roi(frame)
+    roi = frame[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+#    normalize_roi = normalize_image(roi)
+
+    # Get the list of "down" icon templates
+    templates = get_down_icon_templates()
+
+    # Initialize variables to store the best match value and location
+    best_match_val = -1
+    best_match_loc = None
+
+    # Define a range of scales to resize the templates for matching
+    scales = [i for i in range(50, 151, 10)]  # Scales from 0.5x to 1.5x in steps of 0.1x
+
+    # Loop through each scale and template to find the best match
+    for scale in scales:
+        for template in templates:
+
+            # Normalize the template
+#            normalize_template = normalize_image(template)
+
+            # Resize the template based on the current scale
+            resized_template = cv2.resize(template, (int(template.shape[1] * scale / 100), int(template.shape[0] * scale / 100)))
+
+            # Skip the template if it's larger than the ROI
+            if roi.shape[0] < resized_template.shape[0] or roi.shape[1] < resized_template.shape[1]:
+                continue
+
+            # Perform template matching to find the location of the template in the ROI
+            result = cv2.matchTemplate(roi, resized_template, cv2.TM_CCOEFF_NORMED)
             _, max_val, _, max_loc = cv2.minMaxLoc(result)
-            if max_val > self.threshold:
-                self.location = max_loc  # Store the location
-                print(f"Matched with template: {template_path}")  # Print the path of the matched template
-                return True
-        return False
+
+            # Update the best match value and location if the current match is better
+            if max_val > best_match_val:
+                best_match_val = max_val
+                best_match_loc = (max_loc[0] + top_left[0], max_loc[1] + top_left[1])
+
+    # Check if the best match value exceeds the given threshold
+    if best_match_val > threshold:
+        # Print the location and confidence of the detected event
+        print(f"Down event detected at location: {best_match_loc}")
+        print(f"Match confidence: {best_match_val*100:.2f}%")
+        return True, best_match_loc  # Return True and the location if the event is detected
+    else:
+        return False, None  # Return False and None if the event is not detected
--- highlight/project/event/ui/video_processors/event_detection/events/shield_break.py
+++ highlight/project/event/ui/video_processors/event_detection/events/shield_break.py
@@ -1,26 +1,79 @@
-# shield_break.py
-import cv2
-import os
+#shield_break.py
 
-class ShieldBreakEvent:
-    def __init__(self, threshold=0.7):
-        self.threshold = threshold
-        self.templates = self._load_templates("shield_break")
+# Import necessary libraries and modules
+import cv2  # OpenCV library for computer vision tasks
+import os  # Library for interacting with the operating system
+from ...event_detection.regions.center_roi import get_center_roi  # Import the function to get the center region of interest (ROI)
+from ...event_detection.preprocessingsettingsdialog import PreprocessingSettingsDialog
 
-    def _load_templates(self, event_type):
-        template_dir = os.path.join("thumbnail", event_type)
-        return [cv2.imread(os.path.join(template_dir, file), cv2.IMREAD_GRAYSCALE) for file in os.listdir(template_dir) if file.endswith('.png')]
+import logging  # Library for logging information
 
-    def detect(self, frame):
-        roi_gray = self._get_roi_gray(frame)
-        for template in self.templates:
-            result = cv2.matchTemplate(roi_gray, template, cv2.TM_CCOEFF_NORMED)
+# Set the logging level to INFO to display informational messages
+logging.basicConfig(level=logging.INFO)
+
+# Define a function to get templates of the "shield break" icon
+def get_shield_break_templates():
+    # Get the directory path of the current script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # Construct the path to the directory containing "shield break" icon templates
+    template_dir = os.path.join(script_dir, 'thumbnail', 'shield_break')
+    
+    # Read all the image templates from the directory and store them in a list
+    templates = [cv2.imread(os.path.join(template_dir, f)) for f in os.listdir(template_dir) if f.endswith('.png')]
+    
+    # Return the list of templates
+    return templates
+
+# Define a function to detect the "shield break" event in a video frame
+def detect_shield_break_event(frame, threshold=0.8, preprocessing_pipeline=None):
+    # Extract the center region of interest (ROI) from the frame
+    roi, top_left, bottom_right = get_center_roi(frame)
+    roi = frame[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+
+    # Apply preprocessing if a pipeline is provided
+    processed_roi = apply_preprocessing(roi, preprocessing_pipeline)
+
+
+
+    # Get the list of "shield break" icon templates
+    templates = get_shield_break_templates()
+
+    # Initialize variables to store the best match value and location
+    best_match_val = -1
+    best_match_loc = None
+
+    # Define a range of scales to resize the templates for matching
+    scales = [i for i in range(50, 151, 10)]  # Scales from 0.5x to 1.5x in steps of 0.1x
+
+    # Loop through each scale and template to find the best match
+    for scale in scales:
+        for template in templates:
+            # Resize the template based on the current scale
+            resized_template = cv2.resize(template, (int(template.shape[1] * scale / 100), int(template.shape[0] * scale / 100)))
+
+            # Preprocess the template
+            processed_template = apply_preprocessing(resized_template)
+
+
+            # Skip the template if it's larger than the ROI
+            if roi.shape[0] < template.shape[0] or roi.shape[1] < template.shape[1]:
+                continue
+
+            # Perform template matching to find the location of the template in the ROI
+            result = cv2.matchTemplate(roi, template, cv2.TM_CCOEFF_NORMED)
             _, max_val, _, max_loc = cv2.minMaxLoc(result)
-            if max_val > self.threshold:
-                return True, max_loc
-        return False, None
 
-    def _get_roi_gray(self, frame):
-        height, width = frame.shape[:2]
-        roi = frame[int(height*0.45):int(height*0.55), int(width*0.45):int(width*0.55)]
-        return cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+            # Update the best match value and location if the current match is better
+            if max_val > best_match_val:
+                best_match_val = max_val
+                best_match_loc = (max_loc[0] + top_left[0], max_loc[1] + top_left[1])
+
+    # Check if the best match value exceeds the given threshold
+    if best_match_val > threshold:
+        # Print the location and confidence of the detected event
+        print(f"Shield Break detected at location: {best_match_loc}")
+        print(f"Match confidence: {best_match_val*100:.2f}%")
+        return True, best_match_loc  # Return True and the location if the event is detected
+    else:
+        return False, None  # Return False and None if the event is not detected
