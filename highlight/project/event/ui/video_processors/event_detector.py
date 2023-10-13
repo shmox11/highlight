fd7381430b07c299095e017d4a5eaf2397900558
shmox11 <94550618+shmox11@users.noreply.github.com>
2023-10-13 12:51:43 -0400

    Add files via upload

--- highlight/project/event/ui/video_processors/event_detector.py
+++ highlight/project/event/ui/video_processors/event_detector.py
@@ -2,29 +2,30 @@
 from .event_detection.events.down_event import detect_down_event  # Import the function to detect the "down_event"
 from .event_detection.events.shield_break import detect_shield_break_event  # Import the function to detect the "shield_break_event"
 import logging  # Library for logging information
+from .event_detection.preprocess_handler import PreprocessingHandler
+
+
 
 # Set the logging level to INFO to display informational messages
 logging.basicConfig(level=logging.INFO)
 
-# Define the function to detect all events in a frame
-def detect_all_events(frame, threshold=0.8):
+def detect_all_events(frame, threshold=0.8, preprocessing_settings=None):
     # Create an empty dictionary to store detected events
     events = {}
-    
-    # Call the detect_down_event function to check if a "down_event" is detected in the frame
+
+    # Detect the down_event without preprocessing
     is_down_event, location = detect_down_event(frame, threshold)
-    
-    # If a "down_event" is detected, add it to the events dictionary
     if is_down_event:
         events["down_event"] = location
-    
-    is_shield_break_event, location = detect_shield_break_event(frame, threshold, shield_break_preprocessing)
 
-    # If a "shield_event" is detected, add it to the events dictionary
+    # Apply preprocessing to the frame for shield_break_event
+    handler = PreprocessingHandler(preprocessing_settings)
+    processed_frame_for_shield_break_event = handler.apply_preprocessing(frame)
+    is_shield_break_event, location = detect_shield_break_event(processed_frame_for_shield_break_event, threshold, preprocessing_settings)
+
+    # If a "shield_break_event" is detected, add it to the events dictionary
     if is_shield_break_event:
         events["shield_break_event"] = location
-    # Placeholder for adding other event detection functions in the future
-    # ...
-    
+
     # Return the dictionary of detected events
     return events
