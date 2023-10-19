259cf09fb94105a456100f35bd04c782e3eb7bb2
shmox11 <94550618+shmox11@users.noreply.github.com>
2023-10-19 01:17:22 -0400

    Add files via upload

--- highlight/project/event/ui/video_processors/event_detector.py
+++ highlight/project/event/ui/video_processors/event_detector.py
@@ -4,46 +4,47 @@
 from .event_detection.events.down_event import detect_down_event  # Import the function to detect the "down_event"
 from .event_detection.events.shield_break import detect_shield_break_event  # Import the function to detect the "shield_break_event"
 import logging  # Library for logging information
-from .event_detection.preprocess_handler import PreprocessingHandler
 
 # Set the logging level to INFO to display informational messages
 logging.basicConfig(level=logging.INFO)
+print("Logging level set to INFO.")
 
-def detect_all_events(frame, threshold=0.8, preprocessing_settings=None, event_type=None):
+def detect_all_events(frame, threshold=0.8):
     """
-    Detects all events in the given frame.
+    Detects all events in the given frame..
     
     Args:
         frame: The video frame in which to detect events.
         threshold: The threshold for event detection.
-        preprocessing_settings: The settings for preprocessing the frame.
-        event_type: The type of event for which preprocessing should be applied. Default is None.
         
     Returns:
         A dictionary of detected events.
     """
+#    print("Entering detect_all_events function...")
+    
     # Create an empty dictionary to store detected events
     events = {}
+ #   print("Initialized empty events dictionary.")
 
-    # Detect the down_event without preprocessing
+    # Detect the down_event
+ #   print("Detecting down_event...")
     is_down_event, location = detect_down_event(frame, threshold)
     if is_down_event:
         events["down_event"] = location
         print("Down event detected!")
 
-    # Check if preprocessing should be applied for shield_break_event
-    if event_type == "shield_break_event" and preprocessing_settings is not None:
-        print("Applying preprocessing for shield break event...")
-        handler = PreprocessingHandler(preprocessing_settings)
-        processed_frame = handler.apply_preprocessing(frame)
-        is_shield_break_event, location = detect_shield_break_event(processed_frame, threshold)
-    else:
-        is_shield_break_event, location = detect_shield_break_event(frame, threshold)
-
+    # Detect the shield_break_event
+ #   print("Detecting shield_break_event...")
+    is_shield_break_event, location = detect_shield_break_event(frame, threshold)
     # If a "shield_break_event" is detected, add it to the events dictionary
     if is_shield_break_event:
         events["shield_break_event"] = location
         print("Shield break event detected!")
 
     # Return the dictionary of detected events
+ #   print("Returning detected events dictionary.")
+ # Inside detect_all_events function
+    print(f"Down event detected: {is_down_event}, Location: {location}")
+    print(f"Shield break event detected: {is_shield_break_event}, Location: {location}")
+
     return events
